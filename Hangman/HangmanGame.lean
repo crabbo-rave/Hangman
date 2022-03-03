1dba1b311525d0405f0a96d99e62768633223989
crab <92041779+crabbo-rave@users.noreply.github.com>
2022-03-03 18:41:11 -0500

    stuff

--- Hangman/HangmanGame.lean
+++ Hangman/HangmanGame.lean
@@ -17,9 +17,15 @@ def convertToHiddenWord (str : String) : List (Char × Char) :=
   |>.toList 
   |>.map (fun c => (c, convertToHiddenChar c))
 
-def joinHiddenWord (state : HangmanGame) :=
+def joinHiddenWord (state : HangmanGame) : String :=
   state.zippedWord
   |>.foldr (fun t accstr => (toString t.snd) ++ accstr) ""
+
+def revealLetter (state : HangmanGame) (c : Char) : HangmanGame :=
+  let newZippedWords := 
+    state.zippedWord
+    |>.map (fun t => if t.fst = c then (t.fst, c) else (t.fst, t.snd))
+  { state with zippedWord := newZippedWords }
 
 end HangmanGame
 
@@ -34,3 +40,5 @@ end HangmanGame
 def abj := HangmanGame.init "apple bottom jeans"
 
 #eval abj.joinHiddenWord
+
+#eval abj |>.revealLetter 'a' |>.joinHiddenWord
